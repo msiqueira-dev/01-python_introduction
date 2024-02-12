be5b37ed7cbb428440c71e30699e80c4998da26e
msiquera-dev <marcusviniciusb.siqueira@hotmail.com>
2024-02-12 14:52:31 -0300

    Adicionando comentario

--- introduction.py
+++ introduction.py
@@ -1,4 +1,9 @@
-#This is a comment and will be ignored by Python.
+# PT-BR: 
+#   Isso é um comentário e será ignorado pelo Python
+#   Esse Programa tem como finalidade imprimir uma mensagem na tela e ser o primeiro programa em Python
+# EN: 
+#   This is a comment and will be ignored by Python.
+#   This program has the objective of printing a message in the screen and be the first python program
 
 print('Hello World')
 mensagem = 'Hello python'
